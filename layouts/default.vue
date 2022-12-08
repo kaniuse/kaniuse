172a6e5256402c3a2368951922111c2ad95c6bfb
STRRL <im@strrl.dev>
2022-12-08 20:43:47 +0800

    feat: link to github
    Signed-off-by: STRRL <im@strrl.dev>

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,18 +1,25 @@
 <template>
   <div>
     <div class="navbar bg-neutral text-neutral-content">
-      <a class="btn btn-ghost normal-case text-xl" href="/">Kaniuse</a>
-      <div class="tabs tabs-boxed">
-        <div class="tab" :class="{ 'tab-active': $route.path.startsWith('/kind') }">
-          <NuxtLink href="/kinds">Kinds</NuxtLink>
+      <div class="flex-1">
+        <a class="btn btn-ghost normal-case text-xl" href="/">Kaniuse</a>
+        <div class="tabs tabs-boxed">
+          <div class="tab" :class="{ 'tab-active': $route.path.startsWith('/kind') }">
+            <NuxtLink href="/kinds">Kinds</NuxtLink>
+          </div>
+          <div class="tab" :class="{ 'tab-active': $route.path.startsWith('/field') }">
+            <NuxtLink href="/fields">Fields</NuxtLink>
+          </div>
         </div>
-        <div class="tab" :class="{ 'tab-active': $route.path.startsWith('/field') }">
-          <NuxtLink href="/fields">Fields</NuxtLink>
-        </div>
+      </div>
+
+      <div class="flex-none">
+        <a class="btn btn-ghost normal-case text-xl" target="_blank" href="https://github.com/kaniuse/kaniuse"
+          >View On GitHub</a
+        >
       </div>
     </div>
     <div class="container mx-auto">
-      {{}}
       <slot />
     </div>
   </div>
